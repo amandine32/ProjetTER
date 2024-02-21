4ec0e5f7f24fececa7b5af1d8f541d2ee906419b
amandine <amandinebremont@hotmailfr>
2024-02-21 18:36:48 +0100

    selection multiple des users

--- src/vue/postitVue.php
+++ src/vue/postitVue.php
@@ -18,39 +18,38 @@
     <div class="d1">
     <h1>New post-it:</h1>
         <div>
-            <form action="../controleur/ajoutpostitControleur.php" method="post" class="formulaire">
-                <!-- Champ pour le titre -->
-                <label for="titre">Titre :</label><br>
-                <input type="text" id="titre" name="titre" maxlength="150" required>
-                <br>
+        <form action="../controleur/ajoutpostitControleur.php" method="post" class="formulaire">
+    <!-- Champ pour le titre -->
+    <label for="titre">Titre :</label><br>
+    <input type="text" id="titre" name="titre" maxlength="150" required>
+    <br>
 
-                  <!-- Champ pour le pseudo -->
-                <label for="pseudo"> Pseudo:</label><br>
-                <input type="text" id="pseudo" name="pseudo" class="pseudo" required><br>
+    <!-- Champ pour le pseudo -->
+    <label for="pseudo">Pseudo:</label><br>
+    <input type="text" id="pseudo" name="pseudo" class="pseudo" required><br>
 
-                <!-- Champ pour la date d'ajout -->
-                <label for="datedecreation">Date d'ajout :</label><br>
-                <?php $datedecreation = date('d-m-Y'); ?>
-                <input type="text" id="datedecreation" name="datedecreation" value="<?php echo $datedecreation; ?>" readonly>
-                <br>
-                <label for="users">Partager avec :</label><br>
-                
-                <select name="users[]" id="users" multiple>
-                    <?php foreach ($users as $user) : ?>
-                        <option value="<?php echo $user['IDUSER']; ?>"><?php echo $user['PSEUDO']; ?></option>
-                    <?php endforeach; ?>
-                </select>
+    <!-- Champ pour la date d'ajout -->
+    <label for="datedecreation">Date d'ajout :</label><br>
+    <?php $datedecreation = date('d-m-Y'); ?>
+    <input type="text" id="datedecreation" name="datedecreation" value="<?php echo $datedecreation; ?>" readonly>
+    <br>
 
+    <!-- Liste des utilisateurs avec cases à cocher -->
+    <label for="users">Partager avec :</label><br>
+    <?php foreach ($users as $user) : ?>
+        <input type="checkbox" id="user_<?php echo $user['IDUSER']; ?>" name="users[]" value="<?php echo $user['IDUSER']; ?>">
+        <label for="user_<?php echo $user['IDUSER']; ?>"><?php echo $user['PSEUDO']; ?></label><br>
+    <?php endforeach; ?>
 
+    <!-- Champ pour le libellé du post-it -->
+    <label for="libelle">Libellé:</label><br>
+    <input type="text" id="libelle" name="libelle" class="libelle" required>
+    <br><br>
 
-                <!-- Champ pour le libellé du post-it -->
-                <label for="libelle"> Libellé:</label><br>
-                <input type="text" id="libelle" name="libelle" class="libelle" required>
-                <br><br>
+    <!-- Bouton d'enregistrement -->
+    <input type="submit" value="Enregistrer">
+</form>
 
-                <!-- Bouton d'enregistrement -->
-                <input type="submit" value="Enregistrer">
-            </form>
         </div>
     </div>
 
