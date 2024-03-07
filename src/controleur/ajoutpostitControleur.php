0a536ee4d1cdff09b037f8b251981b67dd5617f4
amandine <amandinebremont@hotmailfr>
2024-03-07 14:43:01 +0100

    bdd

--- src/controleur/ajoutpostitControleur.php
+++ src/controleur/ajoutpostitControleur.php
@@ -15,37 +15,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_SESSION['userId'])) {
         $userId = $_SESSION['userId']; 
 
-        // Récupération des champs du formulaire
+
         $titre = $_POST['titre'];
         $libelle = $_POST['libelle'];
-        $pseudo = $_POST['pseudo']; // Note: Vous pourriez vouloir utiliser l'IDUSER pour récupérer le pseudo de l'utilisateur
+        $pseudo = $_POST['pseudo']; 
         $datedecreation = $_POST['datedecreation'];
 
-        // Récupération des utilisateurs sélectionnés pour le partage
         $selectedUsers = isset($_POST['users']) ? $_POST['users'] : [];
 
         try {
-            // Insertion du post-it dans la base de données avec l'IDUSER
+   
             $inserted = $postitModel->createPostit($titre, $libelle, $pseudo, $datedecreation, $userId);
 
             if ($inserted) {
-                // Récupération de l'ID du dernier post-it inséré
                 $lastInsertedPostitId = $postitModel->getLastInsertId();
 
-                // Insertion des partages dans la base de données
                 foreach ($selectedUsers as $userId) {
                     $postitModel->createPartage($userId, $lastInsertedPostitId);
                 }
 
-                // Redirection vers la page d'accueil en cas de succès
                 header("Location: /ProjetTER/src/vue/AccueilVue.php");
                 exit();
             } else {
-                // Affichage d'un message d'erreur si l'insertion a échoué
+
                 $errorMessage = "Erreur lors de l'enregistrement du post-it.";
             }
         } catch (Exception $e) {
-            // Gestion des exceptions
+
             $errorMessage = "Une erreur s'est produite : " . $e->getMessage();
         }
     } else {
@@ -53,6 +49,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-// Inclusion de la vue pour afficher le formulaire ou les messages d'erreur
+
 include_once('../vue/postitVue.php');
 ?>
