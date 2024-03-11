f857061bf75be2f96a3ae9e654407f37aaa20141
amandine <amandinebremont@hotmailfr>
2024-03-11 10:28:51 +0100

    mdp

--- src/vue/resetPasswordQuestionVue.php
+++ src/vue/resetPasswordQuestionVue.php
@@ -9,6 +9,9 @@
 <body>
     <form action="index.php?page=resetPassword" method="post">
         <input type="email" name="mail" placeholder="Votre email" required>
+        <select id="questionSec" name="questionSec" required>
+        <option value="option1">Quel est le prénom de votre meilleur ami?</option>
+        <option value="option1">Quelle est votre ville de naissance?</option>
         <input type="text" name="reponseSec" placeholder="Réponse à votre question secrète" required>
         <button type="submit">Vérifier</button>
     </form>
